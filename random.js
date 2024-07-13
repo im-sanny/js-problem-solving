472f03d38ebaf2e0840b83f996fd16b19c76b571
im-sanny <raselparvezsanny@gmail.com>
2024-07-13 18:36:50 +0600

    add: get random number method added successfully

---
+++ random.js
@@ -0,0 +1,17 @@
+// print a number between 1 to 6
+
+// function getRandomNumber (min, max) {
+//     return Math.floor(Math.random() * (max - min + 1)) + 1;
+// }
+
+// console.log(getRandomNumber(1, 6));
+
+// function number (min, max){
+//     return Math.floor(Math.random() * (max - min + 1)) + 1;
+// }
+// console.log(number(1, 8));
+
+function numb (min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + 1;
+}
+console.log('numb', numb(1, 7));
