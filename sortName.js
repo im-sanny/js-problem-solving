971469f93bf96eaa9cf3652310c8640103ca6669
im-sanny <raselparvezsanny@gmail.com>
2024-08-01 23:04:30 +0600

    code updated

--- sortName.js
+++ sortName.js
@@ -1,5 +1,8 @@
 const sortName = ["lalu", "kalu", "vulu", "iku", "piku", "aku"];
-console.log(sortName.sort());
+// console.log(sortName.sort());
 sortName.push("alu");
 sortName.shift();
 console.log(sortName.sort());
+console.log(sortName.pop());
+console.log(sortName.push("lol"));
+console.log(sortName);
