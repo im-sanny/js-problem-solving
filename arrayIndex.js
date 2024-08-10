e482477ddf51ebf93cea0fa6b86c36afed14524f
im-sanny <raselparvezsanny@gmail.com>
2024-08-10 22:02:14 +0600

    code updated

--- arrayIndex.js
+++ arrayIndex.js
@@ -1,8 +1,8 @@
 const numbers = [23, 453, 29, 97, 68, 56];
-// console.log(numbers[5]);
+console.log(numbers[5]);
 
-// let setNum = 23;
-// setNum = 777;
+let setNum = 23;
+setNum = 777;
 
 numbers[1] = 9999999;
 console.log(numbers);
