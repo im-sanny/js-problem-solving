09140657d640c688f08a56f250151bb4158649e9
im-sanny <raselparvezsanny@gmail.com>
2024-08-10 22:06:23 +0600

    code update

--- leapYear.js
+++ leapYear.js
@@ -1,13 +1,13 @@
 // check the year its leap year or not 
 
-// function isLeapYear(year){
-//     if ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
-//         console.log(`${year} is a leap year!`);
-//     }else{
-//         console.log(`${year} is not a leap year!`);
-//     }
-// }
-// isLeapYear(2032)
+function isLeapYear(year){
+    if ((year % 400 === 0) || ((year % 4 === 0) && (year % 100 !== 0))) {
+        console.log(`${year} is a leap year!`);
+    }else{
+        console.log(`${year} is not a leap year!`);
+    }
+}
+isLeapYear(2032)
 
 
 function isLeapYear(year){
